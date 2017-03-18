bdb1216ec58d2d14d96de7be5c8a366470f724fa
Bishal Santra <bs.bytestorm@gmail.com>
2017-03-18 16:45:24 +0530

    blah

---
+++ train_dense.py
@@ -0,0 +1,32 @@
+'''
+Deep Learning Programming Assignment 2
+--------------------------------------
+Name:
+Roll No.:
+
+======================================
+Complete the functions in this file.
+Note: Do not change the function signatures of the train
+and test functions
+'''
+import numpy as np
+
+
+def train(trainX, trainY):
+    '''
+    Complete this function.
+    '''
+
+
+def test(testX):
+    '''
+    Complete this function.
+    This function must read the weight files and
+    return the predicted labels.
+    The returned object must be a 1-dimensional numpy array of
+    length equal to the number of examples. The i-th element
+    of the array should contain the label of the i-th test
+    example.
+    '''
+
+    return np.zeros(testX.shape[0])
