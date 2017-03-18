de5f9eb804f05f0d52c65430b69981d01bb5b712
Bishal Santra <bs.bytestorm@gmail.com>
2017-03-19 02:09:06 +0530

    2K

--- train_cnn.py
+++ train_cnn.py
@@ -1,15 +1,139 @@
 '''
 Deep Learning Programming Assignment 2
 --------------------------------------
-Name:
-Roll No.:
+Name: Bishal Santra
+Roll No.: 12EC35001
 
 ======================================
 Complete the functions in this file.
 Note: Do not change the function signatures of the train
 and test functions
 '''
+# Imports for CNN
 import numpy as np
+import os
+import train_dense
+import train_cnn
+
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+import numpy as np
+import tensorflow as tf
+
+from tensorflow.contrib import learn
+from tensorflow.contrib.learn.python.learn.estimators import model_fn as model_fn_lib
+
+tf.logging.set_verbosity(tf.logging.INFO)
+
+# Imports for downloader
+
+import requests
+import os
+
+def downloadData_CNN():
+    http_proxy  = "http://10.3.100.207:8080"
+    https_proxy = "http://10.3.100.207:8080"
+    ftp_proxy   = "http://10.3.100.207:8080"
+
+    proxyDict = { 
+                  "http"  : http_proxy, 
+                  "https" : https_proxy, 
+                  "ftp"   : ftp_proxy
+                }
+    
+    base = 'https://raw.githubusercontent.com/studiobytestorm/DL-ASGN3-CNN/master/tmp/'
+
+    weightList = ['checkpoint', 'events.out.tfevents.1489824501.MSI', 'graph.pbtxt', 'model.ckpt-1.index',\
+                  'model.ckpt-1.meta', 'model.ckpt-2000.data-00000-of-00001', 'model.ckpt-2000.index', 'model.ckpt-2000.meta']
+
+    path_root = './dtmp/'
+    if not os.path.exists(path_root):
+        os.makedirs(path_root)
+    
+    count = 0
+    total = len(weightList)
+    
+    print('Downloading CNN Weights: 1 of the files (12 MB in size) will take some time!')
+    for name in weightList:
+        url = base + name
+        if os.path.isfile(path_root + name):
+            print('File exists: [{}] -> Skip Download'.format(name))
+            continue
+        print('Downloading File: ', name)
+        stream = requests.get(url, proxyDict)
+        np_f = open(path_root + name, 'wb')
+        np_f.write(stream.content)
+        np_f.close()
+        print(count+1,'/',total,' Complete')
+        count += 1
+        
+    print('Download Complete')
+
+    
+def cnn_model_fn(features, labels, mode):
+    """Model function for CNN."""
+    # Input Layer
+    input_layer = tf.reshape(features, [-1, 28, 28, 1])
+
+    # Convolutional Layer #1
+    conv1 = tf.layers.conv2d(
+      inputs=input_layer,
+      filters=32,
+      kernel_size=[5, 5],
+      padding="same",
+      activation=tf.nn.relu)
+
+    # Pooling Layer #1
+    pool1 = tf.layers.max_pooling2d(inputs=conv1, pool_size=[2, 2], strides=2)
+
+    # Convolutional Layer #2 and Pooling Layer #2
+    conv2 = tf.layers.conv2d(
+      inputs=pool1,
+      filters=64,
+      kernel_size=[5, 5],
+      padding="same",
+      activation=tf.nn.relu)
+    pool2 = tf.layers.max_pooling2d(inputs=conv2, pool_size=[2, 2], strides=2)
+
+    # Dense Layer
+    pool2_flat = tf.reshape(pool2, [-1, 7 * 7 * 64])
+    dense = tf.layers.dense(inputs=pool2_flat, units=1024, activation=tf.nn.relu)
+    dropout = tf.layers.dropout(
+      inputs=dense, rate=0.4, training=mode == learn.ModeKeys.TRAIN)
+
+    # Logits Layer
+    logits = tf.layers.dense(inputs=dropout, units=10)
+
+    loss = None
+    train_op = None
+
+    # Calculate Loss (for both TRAIN and EVAL modes)
+    if mode != learn.ModeKeys.INFER:
+        onehot_labels = tf.one_hot(indices=tf.cast(labels, tf.int32), depth=10)
+        loss = tf.losses.softmax_cross_entropy(
+            onehot_labels=onehot_labels, logits=logits)
+
+    # Configure the Training Op (for TRAIN mode)
+    if mode == learn.ModeKeys.TRAIN:
+        train_op = tf.contrib.layers.optimize_loss(
+            loss=loss,
+            global_step=tf.contrib.framework.get_global_step(),
+            learning_rate=0.001,
+            optimizer="SGD")
+
+    # Generate Predictions
+    predictions = {
+      "classes": tf.argmax(
+          input=logits, axis=1),
+      "probabilities": tf.nn.softmax(
+          logits, name="softmax_tensor")
+    }
+
+    # Return a ModelFnOps object
+    return model_fn_lib.ModelFnOps(
+      mode=mode, predictions=predictions, loss=loss, train_op=train_op)
 
 
 def train(trainX, trainY):
@@ -17,6 +141,15 @@ def train(trainX, trainY):
     Complete this function.
     Note: Load model in folder ./tmp/
     '''
+    
+    train_data = np.asarray(trainX, dtype=np.float32) # Returns np.array
+    train_labels = np.asarray(trainY, dtype = np.float32)
+
+    # Estimator
+    mnist_classifier = learn.Estimator(
+      model_fn=cnn_model_fn, model_dir="./tmp")
+    
+    mnist_classifier.fit(x=train_data, y=train_labels, batch_size=100, steps=2000)
 
 
 def test(testX):
@@ -32,5 +165,17 @@ def test(testX):
     
     Note: Download and Load model in folder ./dtmp/
     '''
+    # Download the model
+    downloadData_CNN()
+    
+    # Predict    
+    eval_data = np.asarray(testX, dtype = np.float32) # Returns np.array
 
-    return np.zeros(testX.shape[0])
+    # Estimator
+    mnist_classifier = learn.Estimator(
+      model_fn=cnn_model_fn, model_dir="./dtmp")
+    
+    yd_hat = list(mnist_classifier.predict(x=eval_data))
+    labels_hat = np.array([what['classes'] for what in yd_hat])
+
+    return labels_hat
--- train_dense.py
+++ train_dense.py
@@ -1,8 +1,8 @@
 '''
 Deep Learning Programming Assignment 2
 --------------------------------------
-Name:
-Roll No.:
+Name: Bishal Santra
+Roll No.: 12EC35001
 
 ======================================
 Complete the functions in this file.
@@ -10,14 +10,108 @@ Note: Do not change the function signatures of the train
 and test functions
 '''
 import numpy as np
+import os
+import tensorflow as tf
 
+def modify(X, Y):
+    X_mod = np.ndarray((X.shape[0], 28*28))
+    for i in range(X.shape[0]):
+        X_mod[i, :, None] = np.reshape(X[i, :, :], (28*28, 1))
+    
+    Y_mod = np.zeros((X.shape[0], 10))
+    for i in range(Y.shape[0]):
+        Y_mod[i, Y[i]] = 1
+    print(X_mod.shape)
+    print(Y_mod.shape)
+    
+    return X_mod, Y_mod
+
+def modifyX(X):
+    X_mod = np.ndarray((X.shape[0], 28*28))
+    for i in range(X.shape[0]):
+        X_mod[i, :, None] = np.reshape(X[i, :, :], (28*28, 1))
+    
+    
+    return X_mod
 
 def train(trainX, trainY):
     '''
     Complete this function.
     '''
+    
+    trainX_mod, trainY_mod = modify(trainX, trainY)
+    testX_mod, testY_mod = modify(testX, testY)
+    learning_rate = 0.001
+    training_epochs = 5
+    batch_size = 100
 
+    input_dim = 28*28
 
+    n_input = input_dim
+    n_hidden_1 = int(input_dim/2) + 5
+    n_output = 10
+
+    x = tf.placeholder('float', [None, n_input])
+    y = tf.placeholder('float', [None, n_output])
+
+    def mperceptron(x, weights, biases):
+        layer_1 = tf.add(tf.matmul(x, weights['h1']), biases['b1'])
+        layer_1 = tf.nn.relu(layer_1)
+
+        # layer_2 = tf.add(tf.matmul(layer_1, weights['h2']), biases['b2'])
+        # layer_2 = tf.nn.relu(layer_2)
+
+        out_layer = tf.matmul(layer_1, weights['out']) + biases['out']
+        return out_layer
+
+    weights = {
+        'h1' : tf.Variable(tf.random_normal([n_input, n_hidden_1], 0, 0.001)),
+        # 'h2': tf.Variable(tf.random_normal([n_hidden_1, n_hidden_2], 0, 0.001)),
+        'out' : tf.Variable(tf.random_normal([n_hidden_1, n_output], 0, 0.001))
+    }
+
+    biases = {
+        'b1' : tf.Variable(tf.random_normal([n_hidden_1], 0, 0.001)),
+        # 'b2': tf.Variable(tf.random_normal([n_hidden_2], 0, 0.001)),
+        'out' : tf.Variable(tf.random_normal([n_output], 0, 0.001))
+    }
+
+    pred = mperceptron(x, weights, biases)
+
+    cost = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits=pred, labels=y))
+    optimizer = tf.train.AdamOptimizer(learning_rate=learning_rate).minimize(cost)
+
+    init = tf.global_variables_initializer()
+
+    sess = tf.Session()    
+    sess.run(init)
+    
+    epochs = 5
+    batches = 100
+    batch_size = int((trainX_mod.shape[0])/batches)
+
+    for _ in range(epochs):
+        for bx in range(batches):
+            batch_indices = list(range((bx*batch_size), (bx + 1)*batch_size))
+            _, c = sess.run([optimizer, cost], \
+                            feed_dict={x: trainX_mod[batch_indices, :],\
+                                       y: trainY_mod[batch_indices]})
+    
+    
+    w_h1 = sess.run(weights)['h1']
+    w_out = sess.run(weights)['out']
+    b_h1 = sess.run(biases)['b1']
+    b_out = sess.run(biases)['out']
+    
+    pickle.dump({
+            'w_h1': w_h1,
+            'w_out': w_out,
+            'b_h1': b_h1,
+            'b_out': b_out
+        }, open('./saved_dense_matrices.p', 'wb'))
+    
+    sess.close()
+    
 def test(testX):
     '''
     Complete this function.
@@ -29,4 +123,55 @@ def test(testX):
     example.
     '''
 
-    return np.zeros(testX.shape[0])
+    testX_mod = modifyX(testX)
+    learning_rate = 0.001
+    training_epochs = 5
+    batch_size = 100
+
+    input_dim = 28*28
+
+    n_input = input_dim
+    n_hidden_1 = int(input_dim/2) + 5
+    n_output = 10
+
+    x = tf.placeholder('float', [None, n_input])
+    y = tf.placeholder('float', [None, n_output])
+
+    def mperceptron(x, weights, biases):
+        layer_1 = tf.add(tf.matmul(x, weights['h1']), biases['b1'])
+        layer_1 = tf.nn.relu(layer_1)
+
+        # layer_2 = tf.add(tf.matmul(layer_1, weights['h2']), biases['b2'])
+        # layer_2 = tf.nn.relu(layer_2)
+
+        out_layer = tf.matmul(layer_1, weights['out']) + biases['out']
+        return out_layer
+
+    loader = pickle.load(open('./saved_dense_matrices.p', 'rb'))
+    weights = {
+        'h1' : tf.Variable(loader['w_h1']),
+        # 'h2': tf.Variable(tf.random_normal([n_hidden_1, n_hidden_2], 0, 0.001)),
+        'out' : tf.Variable(loader['w_out'])
+    }
+
+    biases = {
+        'b1' : tf.Variable(loader['b_h1']),
+        # 'b2': tf.Variable(tf.random_normal([n_hidden_2], 0, 0.001)),
+        'out' : tf.Variable(loader['b_out'])
+    }
+
+    pred = mperceptron(x, weights, biases)
+
+    cost = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits=pred, labels=y))
+    optimizer = tf.train.AdamOptimizer(learning_rate=learning_rate).minimize(cost)
+
+    init = tf.global_variables_initializer()
+
+    sess = tf.Session()    
+    sess.run(init)
+    
+    y_hat = sess.run(pred, feed_dict={x: testX_mod})
+    
+    sess.close()
+    
+    return np.argmax(y_hat, axis=1)
